32cdd4e1bbd07a48ab2391e23ccb1d7e211d332b
GorvatMaks <140732175+GorvatMaks@users.noreply.github.com>
2023-08-03 19:42:06 +0300

    Add files via upload

---
+++ logika/m2/les1/vsc crazy people/main.py
@@ -0,0 +1,30 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtCore import QtMsgType
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+from random import randint
+
+app = QApplication([])
+main_window = QWidget()
+
+button = QPushButton("Згенерувати")
+text = QLabel("Натисни щоб дізнатися переможця")
+winer = QLabel("?")
+
+
+line = QVBoxLayout()
+line.addWidget(text, alignment=Qt.AlignCenter)
+line.addWidget(winer, alignment=Qt.AlignCenter)
+line.addWidget(button, alignment=Qt.AlignCenter)
+
+def win():
+    ran = randint(1, 1000)
+    winer.setText(str(ran))
+    button.hide()
+
+button.clicked.connect(win)    
+
+
+main_window.setLayout(line)
+
+main_window.show()
+app.exec_()
